63a63fecc12fa8ee2f69503108439a6fc0843d54
DennysSevilla <dennyssevilla2@gmail.com>
2025-11-06 14:20:46 -0500

    funciones.h

--- funciones.h
+++ funciones.h
@@ -3,3 +3,4 @@ int registrarProductos(int cantidad, char nombres[][25], float precios[], int ma
 void calcularTotalYPromedio(float precios[], int cantidad, int stock[], float resultados[]);
 void encontrarExtremos(float precios[], char nombres[][25], int cantidad, 
                        char masCaro[], float resultadosCaro[], char masBarato[], float resultadosBarato[]);
+void buscarProducto(char nombreBuscado[], char nombres[][25], float precios[], int stock[], int cantidad);
